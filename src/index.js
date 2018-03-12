5de67b98c70c0ca2354fee6218c7923440f12845
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-03-12 22:26:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,24 +4,24 @@ module.exports = function longestConsecutiveLength(array) {
   if (a > b) return 1;
   if (a < b) return -1;
 }
-array.sort(compare); 
-  let count=-1, thiscount=0;
+array.sort(compare);
+  let count=-1, count1=0;
  for(let i=0; i<array.length;i++){
-  
-  thiscount++;
+
+  count1++;
   if(array[i+1]-array[i]===1)
   continue;
-    
+
    elseif(array[i+1]-array[i]===0)
      {
-     thiscount--;
+     count1--;
       continue;
      }
    else
    {
-     count=(thiscount>count)?thiscount:count;
-     thiscount=0;
+     count=(count1>count)?count1:count;
+     count1=0;
    }
 }
+console.log(count);
   return count;
-}
